475a824dc06b5ec5ea164f44f389243cc26defad
unknown <kertio79@gmail.com>
2025-09-24 11:38:39 +0500

    add NNC Token contract

---
+++ contracts/NNCToken.sol
@@ -0,0 +1,13 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract NNCToken is ERC20 {
+    uint256 public constant INITIAL_BALANCE = 10_000_000;
+
+    constructor() ERC20("No Name Coder", "NNC") {
+        _mint(msg.sender, INITIAL_BALANCE);
+    }
+}
